daf53c22da978344fa200e282a6b84b4b150cb5f
Robert J Woodhead <trebor@animeigo.com>
2017-10-13 09:36:34 -0400

    Delete shave.sh

--- Obsolete/shave.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# cd to directory with 64x64 tiles
-# shave.sh {first frame number} {last frame number} {frame skip} {path-prefix}
-for ((frame=$1; frame<=$2; frame=frame+$3))
-do
-	fnum=$(printf "%06d" $frame)
-	echo $fnum ...
-	mogrify -shave 2x2 $4$fnum*.png
-done
